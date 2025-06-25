f96fff55ef8eddc7cd3c666fb3ca51e0e78041ac
bear <bear@crumpton.org>
2025-06-25 16:03:25 -0600

    restored symbols as used in code

--- buffy.h
+++ buffy.h
@@ -2,7 +2,7 @@
 #ifndef BUFFY_H
 #define BUFFY_H
 
-struct game_state
+struct gs
 {
     int daggerset;
     int flouride;
@@ -10,9 +10,9 @@ struct game_state
     int dagger_effort;
     int flouride_used;
     int bflag;
-};
+} game_state;
 
-struct vampire
+struct bloodsucker
 {
     int age;
     char *name;
@@ -24,7 +24,7 @@ struct vampire
         char *color;
         int health; // health of the fangs from 0 to 10
     } fangs[3];
-};
+} vampire;
 
 int save_game(char *file);
 int load_game(char *file);
