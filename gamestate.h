78ad0f7dc0b76cedc73d3fe50383f2e620f84c7c
bear <bear@crumpton.org>
2025-06-26 09:33:32 -0600

    beautified header file

--- gamestate.h
+++ gamestate.h
@@ -5,6 +5,6 @@
 #define MINOR 0
 #define PATCH 0
 
-#define GAMECODE 0x62746664 /* buffy the flouride dispenser */
+#define GAMECODE 0x62746664	/* buffy the flouride dispenser */
 
-#endif // GAMESTATE_H
+#endif	/* GAMESTATE_H */
