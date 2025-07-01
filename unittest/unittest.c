e2da297c3ed47b6c4acced95931b45ad9dd68de1
bear <bear@crumpton.org>
2025-07-01 12:32:14 -0600

    removed call of load_game

--- unittest/unittest.c
+++ unittest/unittest.c
@@ -236,7 +236,6 @@ main()
 	    (NULL == CU_add_test(pSuite, "test of print_flouride_info()", testPRINT_FLOURIDE_INFO)) ||
 	    (NULL == CU_add_test(pSuite, "test of fang_idx_to_name()", testFANG_IDX_TO_NAME)) ||
 		(NULL == CU_add_test(pSuite, "test validate game()", testVALIDATE_GAME_FILE)) ||
-		(NULL == CU_add_test(pSuite, "test of load_game()", testLOADGAME)) ||
 	    (NULL == CU_add_test(pSuite, "test of return_concat_path()", testCONCAT_PATH))) {
 		CU_cleanup_registry();
 		return CU_get_error();
