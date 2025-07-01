b4ce92f664203f0f8844c67e4400761cb40ce4de
bear <bear@crumpton.org>
2025-07-01 16:11:28 -0600

    fixed buffy.c

--- buffy.c
+++ buffy.c
@@ -862,7 +862,7 @@ main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	if (pledge("stdio rpath wpath cpath proc", NULL) == -1)
+	if (pledge("stdio rpath wpath cpath", NULL) == -1)
 		err(1, "pledge");
 #endif
 
